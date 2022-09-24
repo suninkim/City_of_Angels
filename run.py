3bacb4fdadc27813f63183135841542e0a95d5c2
sunin <sunin.kim@naverlabs.com>
2022-09-24 21:56:50 +0900

    first commit

---
+++ run.py
@@ -0,0 +1,13 @@
+import isaacgym
+import argparse
+from utils.run_utils import setup_logger_kwargs
+import os
+import yaml
+import shutil
+from datetime import datetime
+
+def run(args):
+    a=1
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
