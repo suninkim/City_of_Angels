26ee2515967247048401e333f57bb400bb524f84
suninkim <qscesz300@naver.com>
2024-09-27 21:38:19 +0900

    add isaaclab setting

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,6 +1,7 @@
 from .coffee_task import RealCoffeeTask
 from .dancing_task import RealDancingTask
 from task_base import *
+from .manager_based import *
 
 real_task_map = {"coffee": RealCoffeeTask, "dancing": RealDancingTask}
 sim_task_map = {"coffee": RealCoffeeTask, "dancing": RealDancingTask}
